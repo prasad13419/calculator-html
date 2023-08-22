994837417729809454b68cd4897d3684f9de2b7e
Prasad <mpkumarasingha@gmail.com>
2023-08-22 22:47:04 +0530

    my first commit

---
+++ script/script.js
@@ -0,0 +1,50 @@
+function fn0() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 0;
+}
+
+function fn1() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 1;
+}
+
+function fn2() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 2;
+}
+
+function fn3() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 3;
+}
+
+function fn4() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 4;
+}
+
+function fn5() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 5;
+}
+
+function fn6() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 6;
+}
+
+function fn7() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 7;
+}
+
+function fn8() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 8;
+}
+
+function fn9() {
+    let previous = document.getElementById("answer").innerHTML;
+    document.getElementById("answer").innerHTML = previous + 9;
+}
+
